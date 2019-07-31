d5c0a97aa1995adbe6d3db33ed9199a80ead1094
Theon Thai Yun Tang <ththai@deloitte.com>
2019-07-31 14:15:06 +0800

    - Initial push with the overall template

---
+++ public/js/customActivity.js
@@ -0,0 +1,109 @@
+define(['postmonger'], function (Postmonger) {
+    'use strict';
+    
+    var connection = new Postmonger.Session();
+    var authTokens = {};
+    var payload = {};
+   
+    // Configuration variables
+    let eventSchema = ''; // Contact:
+    let lastnameSchema = ''; //Last Name || LastName
+    let eventDefinitionKey;
+
+    $(window).ready(onRender);
+
+    connection.on('initActivity', initialize);
+    connection.on('clickedNext', save); //Save function within MC
+   
+
+    function onRender() {
+        // JB will respond the first time 'ready' is called with 'initActivity'
+        connection.trigger('ready');
+        connection.trigger('requestTokens');
+        connection.trigger('requestEndpoints');
+    }
+
+    /**
+     * This function is to pull out the event definition without journey builder.
+     * With the eventDefinitionKey, you are able to pull out values that passes through the journey
+     */
+    connection.trigger('requestTriggerEventDefinition');
+    connection.on('requestedTriggerEventDefinition',function (eventDefinitionModel) {
+        if (eventDefinitionModel) {
+            eventDefinitionKey = eventDefinitionModel.eventDefinitionKey;
+            // console.log('Request Trigger >>>', JSON.stringify(eventDefinitionModel));
+        }
+    });
+    
+    function initialize(data) {
+        if (data) {
+            payload = data;
+        }
+        initialLoad(data);
+        parseEventSchema();
+    }
+
+    /**
+     * Save function is fired off upon clicking of "Done" in Marketing Cloud
+     * The config.json will be updated here if there are any updates to be done via Front End UI
+     */
+    function save() {
+        payload['arguments'].execute.inArguments = [
+            {
+                SAMPLE_PARAM: "SAMPLE PARAM DATA FROM CONFIG.JSON"
+            }
+        ];
+        payload['metaData'].isConfigured = true;
+        connection.trigger('updateActivity', payload);
+    }
+
+    /**
+     * 
+     * @param {*} data
+     * 
+     * This data param is the config json payload that needs to be loaded back into the UI upon opening the custom application within journey builder 
+     */
+    function initialLoad (data) {
+    };
+
+
+    /**
+     * This function is to pull the relevant information to create the schema of the objects
+     * 
+     * This function pulls out the schema for additional customizations that can be used.
+     * This function leverages on the required field of "Last Name" to pull out the overall event schema
+     * 
+     * returned variables of: lastnameSchema , eventSchema.
+     * eventSchema = Case:Contact:
+     * lastnameSchema = Case:Contact:<last_name_schema>
+     * 
+     * View the developer console in chrome upon opening of application in MC Journey Builder for further clarity.
+     */
+    function parseEventSchema() {
+        // Pulling data from the schema
+        connection.trigger('requestSchema');
+        connection.on('requestedSchema', function (data) {
+            // save schema
+            let dataJson = data['schema'];
+
+            for (let i = 0; i < dataJson.length; i++) {
+
+                // Last name schema and creation of event schema
+                // Last name is a required field in SF so this is used to pull the event schema
+                if (dataJson[i].key.toLowerCase().replace(/ /g, '').indexOf("lastname") !== -1) {
+                    let splitArr = dataJson[i].key.split(".");
+                    lastnameSchema = splitArr[splitArr.length - 1];
+                    console.log('Last Name Schema >>', lastnameSchema);
+
+                    let splitName = lastnameSchema.split(":");
+                    let reg = new RegExp(splitName[splitName.length - 1], "g");
+                    let oldSchema = splitArr[splitArr.length - 1];
+                    
+                    eventSchema = oldSchema.replace(reg, "");
+                    console.log("Event Schema >>", eventSchema);
+                }
+            }
+
+        });
+    }
+});
---
+++ routes/activity.js
@@ -0,0 +1,146 @@
+'use strict';
+var util = require('util');
+
+// Deps
+const Path = require('path');
+const JWT = require(Path.join(__dirname, '..', 'lib', 'jwtDecoder.js'));
+var util = require('util');
+let axios = require("axios");
+
+// Global Variables
+const tokenURL = `${process.env.authenticationUrl}/v2/token`;
+
+
+exports.logExecuteData = [];
+function logData(req) {
+    exports.logExecuteData.push({
+        body: req.body,
+        headers: req.headers,
+        trailers: req.trailers,
+        method: req.method,
+        url: req.url,
+        params: req.params,
+        query: req.query,
+        route: req.route,
+        cookies: req.cookies,
+        ip: req.ip,
+        path: req.path,
+        host: req.host,
+        fresh: req.fresh,
+        stale: req.stale,
+        protocol: req.protocol,
+        secure: req.secure,
+        originalUrl: req.originalUrl
+    });
+    console.log("body: " + util.inspect(req.body));
+    console.log("headers: " + req.headers);
+    console.log("trailers: " + req.trailers);
+    console.log("method: " + req.method);
+    console.log("url: " + req.url);
+    console.log("params: " + util.inspect(req.params));
+    console.log("query: " + util.inspect(req.query));
+    console.log("route: " + req.route);
+    console.log("cookies: " + req.cookies);
+    console.log("ip: " + req.ip);
+    console.log("path: " + req.path);
+    console.log("host: " + req.host);
+    console.log("fresh: " + req.fresh);
+    console.log("stale: " + req.stale);
+    console.log("protocol: " + req.protocol);
+    console.log("secure: " + req.secure);
+    console.log("originalUrl: " + req.originalUrl);
+}
+
+/*
+ * POST Handler for / route of Activity (this is the edit route).
+ */
+exports.edit = function (req, res) {
+    // Data from the req and put it in an array accessible to the main app.
+    //console.log( req.body );
+    logData(req);
+    res.send(200, 'Edit');
+};
+
+/*
+ * POST Handler for /save/ route of Activity.
+ */
+exports.save = function (req, res) {
+    // Data from the req and put it in an array accessible to the main app.
+    //console.log( req.body );
+    logData(req);
+    res.send(200, 'Save');
+};
+
+/*
+ * POST Handler for /execute/ route of Activity.
+ */
+exports.execute = function (req, res) {
+    JWT(req.body, process.env.jwtSecret, (err, decoded) => {
+        // verification error -> unauthorized request
+        if (err) {
+            console.error(err);
+            return res.status(401).end();
+        }
+
+        if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
+            console.log('##### decoded ####=>', decoded);
+            res.send(200, 'Execute');
+        } else {
+            console.error('inArguments invalid.');
+            return res.status(400).end();
+        }
+
+    });
+};
+
+
+/*
+ * POST Handler for /publish/ route of Activity.
+ */
+exports.publish = function (req, res) {
+    //console.log( req.body );
+    logData(req);
+    res.send(200, 'Publish');
+};
+
+
+/*
+ * POST Handler for /validate/ route of Activity.
+ */
+exports.validate = function (req, res) {
+    // Data from the req and put it in an array accessible to the main app.
+    //console.log( req.body );
+    logData(req);
+    res.send(200, 'Validate');
+};
+
+
+/*
+ * POST Handler for /Stop/ route of Activity.
+ */
+exports.stop = function (req, res) {
+    // Data from the req and put it in an array accessible to the main app.
+    //console.log( req.body );
+    logData(req);
+    res.send(200, 'Stop');
+};
+
+
+/**
+ * This function relies on the env variables to be set
+ * 
+ * clientId
+ * clientSecret
+ */
+function retrieveToken () {
+    axios.post(tokenURL, { // Retrieving of token
+        grant_type: 'client_credentials',
+        client_id: process.env.clientId,
+        client_secret: process.env.clientSecret
+    })
+    .then(function (response) {
+        return response.data['access_token'];
+    }).catch(function (error) {
+        return error;
+    });
+}
